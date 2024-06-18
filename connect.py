0148b52b74e30fd1727031102cf2baf3d21e027d
Vignesh-ViggyPiggy <145573332+Vignesh-ViggyPiggy@users.noreply.github.com>
2024-06-18 17:42:03 +0530

    Add files via upload

--- connect.py
+++ connect.py
@@ -1,10 +1,14 @@
 import mysql.connector
 
-database = mysql.connector.connect(
-    host = "127.0.0.1",
-    user = "project",
-    password = "dawg",
-    database = "project_database"
-)
-if database.is_connected():
-    print("connected successfully")
+def database_connect():
+    database = mysql.connector.connect(
+        host = "127.0.0.1",
+        user = "project",
+        password = "dawg",
+        database = "project_database"
+    
+    )
+    return database
+#database = database_connect()
+#if database.is_connected():
+    #print("connected successfully")
--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ from PIL import Image
 from PIL import ImageDraw
 from random import randint
 
-mycursor = connect.database.cursor()
+#mycursor = connect.database.cursor()
 
 def captcha_image_maker(captcha_string):
     image = ImageCaptcha(fonts=["Images\\JandaRomantic.ttf","Images\\Cloudee-JR6wo.otf","Images\\DroidSansMono.ttf","Images\\Moms_typewriter.ttf"])
@@ -27,6 +27,8 @@ def captcha_maker():
     return captcha_string
 
 def matching_scheduler():
+    database = connect.database_connect()
+    mycursor = database.cursor()
     list_workers_query = "select username from login where account_type = 'Worker'"
     mycursor.execute(list_workers_query)
     list_workers = mycursor.fetchall()
@@ -46,13 +48,15 @@ def matching_scheduler():
             
             adding_query = "insert into matching values('{a}','{b}');".format(a=list_clients[client_number][0],b=list_workers[worker_number_scheduled][0])
             mycursor.execute(adding_query)
-            connect.database.commit()      
+            database.commit()      
         else:
             updating_query = "update matching set worker_username = '{b}' where client_username = '{a}';".format(a=list_clients[client_number][0],b=list_workers[worker_number_scheduled][0])
             mycursor.execute(updating_query)
-            connect.database.commit()
+            database.commit()
 
 def garbage_throw(username,garbage_amt):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     exists_query = "select * from garbage where client_username = '{a}';".format(a=username)
     mycursor.execute(exists_query)
     exists = mycursor.fetchall()
@@ -66,18 +70,20 @@ def garbage_throw(username,garbage_amt):
     else:
         garbage_throw_query = "insert into garbage values('{a}',{b});".format(a=username,b=garbage_amt)
         mycursor.execute(garbage_throw_query)
-        connect.database.commit()
+        database.commit()
         messagebox.showinfo("Success","Please wait for collection of garbage")
         screens.garbage_throw_screen(username)
 
 def report_submit(attack_username,defend_username,report_content):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     checker_query = "select * from reports where attack_username = '{a}' and defend_username = '{b}'".format(a=attack_username,b=defend_username)
     mycursor.execute(checker_query)
     checker = mycursor.fetchall()
     if len(checker)==0:
         submit_query = "insert into reports values('{a}','{b}','{c}')".format(a=attack_username,b=defend_username,c=report_content)
         mycursor.execute(submit_query)
-        connect.database.commit()
+        database.commit()
         messagebox.showinfo("Success","Report submitted successfully")
         screens.report_screen(attack_username,defend_username)
     else:
@@ -86,6 +92,8 @@ def report_submit(attack_username,defend_username,report_content):
 
 
 def home_screen_welcome_image(username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     image_select_dict = {"Admin":"Images\\admin_wid.png","Client":"Images\\client_wid.png","Worker":"Images\\worker_bg_wid.png"}
     font = ImageFont.truetype("Images\Cloudee-JR6wo.otf",60)
     background_query = "select Account_type from login where Username = '{a}';".format(a=username)
@@ -98,7 +106,8 @@ def home_screen_welcome_image(username):
     img.save("Images\\welcome_back.png")
 
 def registration_check(username,password,confirm_password,captcha,generated_captcha,account_type):
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     if username=="" or password=="" or confirm_password=="" or captcha=="" or account_type=="":
         messagebox.showerror("Error","Please enter all fields")
         return screens.registration_screen()
@@ -121,19 +130,21 @@ def registration_check(username,password,confirm_password,captcha,generated_capt
         if account_type == "Client":
             
             mycursor.execute("insert into login values('{a}','{b}','{c}')".format(a=username,b=password,c=account_type))
-            connect.database.commit()
+            database.commit()
             messagebox.showinfo("Success","Account successfully created")
             mycursor.execute("insert into points(username) values('{a}')".format(a=username))
-            connect.database.commit()
+            database.commit()
             matching_scheduler()
         else:
             mycursor.execute("insert into request values('{a}','{b}','{c}')".format(a=username,b=password,c=account_type))
-            connect.database.commit()
+            database.commit()
             messagebox.showinfo("Success","Request to register account is successfully sent")
         
         screens.login_screen()
 
 def home_screen_caller_util(username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     account_type_check_query = "select Account_type from login where Username = '{a}';".format(a=username)
     mycursor.execute(account_type_check_query)
     account_type_check = mycursor.fetchall()[0][0]
@@ -145,12 +156,15 @@ def home_screen_caller_util(username):
         screens.worker_screen(username)
 
 def about_worker(username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     about_worker_query = "select other_details.username, other_details.email, other_details.phone, other_details.address from other_details,matching where matching.client_username = '{a}' and matching.worker_username = other_details.username;".format(a=username)
     mycursor.execute(about_worker_query)
     return mycursor.fetchall()
 
 def home_screen_caller(username,password):
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     
     password_check_query = "select Pass_word from login where Username = '{a}';".format(a=username)
     mycursor.execute(password_check_query)
@@ -167,103 +181,122 @@ def home_screen_caller(username,password):
         home_screen_caller_util(username)
 
 def accounts_list():
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     account_list_query = "select login.username, points.reports from login,points where login.username = points.username;"
     mycursor.execute(account_list_query)
     return mycursor.fetchall()
 
 def collection_list(username):
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     collection_query = "select garbage.client_username, garbage.garbage_amt from garbage, matching where matching.worker_username = '{a}' and matching.client_username = garbage.client_username;".format(a=username)
     mycursor.execute(collection_query)
     return mycursor.fetchall()
 
 def request_list():
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     request_query = "select * from request;"
     mycursor.execute(request_query)
     return mycursor.fetchall() 
 
 def reports_list():
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     reports_list_query = "select * from reports;"
     mycursor.execute(reports_list_query)
     return mycursor.fetchall()
 
 def perk_points(username):
-   
+    database = connect.database_connect()
+    mycursor = database.cursor()
     perk_points_query = "select points from points where username = '{a}'".format(a=username)
     mycursor.execute(perk_points_query)
     return mycursor.fetchall()
 
 def excuse_report(username,reported_username,reporting_username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     excuse_query = "delete from reports where attack_username = '{a}' and defend_username = '{b}'".format(a=reporting_username,b=reported_username)
     mycursor.execute(excuse_query)
-    connect.database.commit()
+    database.commit()
     screens.report_list_screen(username)
 
 def accept_report(username,reported_username,reporting_username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     report_add_query = "update points set reports = reports+1 where username = '{a}'".format(a=reported_username)
     mycursor.execute(report_add_query)
-    connect.database.commit()
+    database.commit()
     
     delete_query = "delete from reports where attack_username = '{a}' and defend_username = '{b}'".format(a=reporting_username,b=reported_username)
     mycursor.execute(delete_query)
-    connect.database.commit()
+    database.commit()
     screens.report_list_screen(username)
 
 def view_report(attack_username,defend_username):
-
+    database = connect.database_connect()
+    mycursor = database.cursor()
     view_report_query = "select * from reports where attack_username = '{a}' and defend_username = '{b}'".format(a=attack_username,b=defend_username)
     mycursor.execute(view_report_query)
     return mycursor.fetchall()
 
 def delete_account(username,deleted_user):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     deletion_query = "delete from login where username = '{a}'".format(a=deleted_user)
     mycursor.execute(deletion_query)
-    connect.database.commit()
+    database.commit()
     matching_scheduler()
     messagebox.showinfo("Success","user {a} successfully deleted".format(a=deleted_user))
     screens.delete_user_screen(username)
 
 def accept_collection(worker_username,client_username,garbage_amt):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     recyclable_garbage = randint(1,garbage_amt)
     points = recyclable_garbage//5
     points_query = "update points set points = points+{a} where username = '{b}' or username = '{c}';".format(a=points,b=worker_username,c=client_username)
     mycursor.execute(points_query)
-    connect.database.commit()
+    database.commit()
     delete_garbage_query = "delete from garbage where client_username = '{a}';".format(a=client_username)
     mycursor.execute(delete_garbage_query)
-    connect.database.commit()
+    database.commit()
 
     screens.garbage_collection_screen(worker_username)
 
 def claim_points(username):
-    
+    database = connect.database_connect()
+    mycursor = database.cursor()
     claim_points_query = "update points set points = points-1000 where username = '{a}'".format(a=username)
     mycursor.execute(claim_points_query)
-    connect.database.commit()
+    database.commit()
     messagebox.showinfo("Congratulations!","You have recieved your reward")
     screens.perk_points_screen(username)
 
 def accept_request(new_username,username):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     accept_request_query_1 = "INSERT INTO login select * from request where Username = '{a}';".format(a=new_username)
     mycursor.execute(accept_request_query_1)
-    connect.database.commit()
+    database.commit()
     accept_request_query_2="DELETE FROM request where Username = '{b}';".format(b=new_username)
     mycursor.execute(accept_request_query_2)
-    connect.database.commit()
+    database.commit()
     account_type_query = "select account_type from login where username = '{a}';".format(a = new_username)
     mycursor.execute(account_type_query)
     account_type= mycursor.fetchall()[0][0]
     if account_type == "Worker":
         mycursor.execute("insert into points(username) values('{a}')".format(a=new_username))
-        connect.database.commit()
+        database.commit()
         matching_scheduler()
 
     screens.request_screen(username)
 
 def other_details_updater(username,ans1,ans2,ans3,email,phone,address):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     exists_query = "select * from other_details where username = '{a}'".format(a=username)
     mycursor.execute(exists_query)
     exists = mycursor.fetchall()
@@ -271,16 +304,18 @@ def other_details_updater(username,ans1,ans2,ans3,email,phone,address):
     if len(exists) == 0:
         adding_query = "insert into other_details values('{a}','{b}','{c}','{d}','{e}','{f}','{g}')".format(a=username,b=ans1,c=ans2,d=ans3,e=email,f=phone,g=address)
         mycursor.execute(adding_query)
-        connect.database.commit()
+        database.commit()
         
     else:
         modifying_query = "update other_details set ans1 = '{b}',ans2 = '{c}',ans3 = '{d}',email = '{e}', phone = '{f}',address = '{g}' where username = '{a}' ".format(a=username,b=ans1,c=ans2,d=ans3,e=email,f=phone,g=address)
         mycursor.execute(modifying_query)
-        connect.database.commit()
+        database.commit()
     messagebox.showinfo("Success","Information Updated")
     home_screen_caller_util(username)
 
 def forgot_password_changer(username,question,answer,new_password):
+    database = connect.database_connect()
+    mycursor = database.cursor()
     answer_query = "select ans{a} from other_details where username = '{b}'".format(a=question,b=username)
     mycursor.execute(answer_query)
     answer_check = mycursor.fetchall()
@@ -297,6 +332,6 @@ def forgot_password_changer(username,question,answer,new_password):
            
             update_query = "update login set pass_word = '{a}' where username = '{b}'".format(a=new_password,b=username)
             mycursor.execute(update_query)
-            connect.database.commit()
+            database.commit()
             messagebox.showinfo("success","password successfully updated")
             screens.login_screen()
